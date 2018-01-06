a72b42bd9c98db4ef358cbceeb789f230873d338
vandosant <sjskender@gmail.com>
2018-01-05 23:52:47 -0700

    Add project files

---
+++ src/myNewClass.cpp
@@ -0,0 +1,9 @@
+//
+//  myNewClass.cpp
+//  openFrameworksLib
+//
+//  Created by Scott Skender on 1/5/18.
+//
+//
+
+#include "myNewClass.hpp"
---
+++ src/myNewClass.hpp
@@ -0,0 +1,14 @@
+//
+//  myNewClass.hpp
+//  openFrameworksLib
+//
+//  Created by Scott Skender on 1/5/18.
+//
+//
+
+#ifndef myNewClass_hpp
+#define myNewClass_hpp
+
+#include <stdio.h>
+
+#endif /* myNewClass_hpp */
